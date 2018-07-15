ec212339b53701b628b4be95d4c8fd8c2baa6c42
Henry Summers <ssumo89@gmail.com>
2018-07-15 13:14:26 +0100

    refactor

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
 var app = {};
 
-app.hide1 = function(e){
-	e.preventDefault();
-	$('.menuListArticle1').toggleClass('hidden');
-	$('.menuListArticle2').addClass('hidden');
-	$('.menuListArticle3').addClass('hidden');
-};app.hide2 = function(e){
-	e.preventDefault();
-	$('.menuListArticle2').toggleClass('hidden')
-	$('.menuListArticle1').addClass('hidden');
-	$('.menuListArticle3').addClass('hidden');
-};app.hide3 = function(e){
-// on click of this, add or remove the hidden class to the div
-	e.preventDefault();
-	$('.menuListArticle3').toggleClass('hidden')
-	$('.menuListArticle1').addClass('hidden');
-	$('.menuListArticle2').addClass('hidden');
-};
+app.toggleArticles = function(index){
+	var thisElement = document.getElementsByClassName('menuListArticle')[index];
 
-$('.menuListItem1').on('click', app.hide1);
-$('.menuListItem2').on('click', app.hide2);
-$('.menuListItem3').on('click', app.hide3);
+	if ( $(thisElement).hasClass('showing') ){
+		$('.menuListArticle').removeClass('showing')
+	} else {
+		$('.menuListArticle').removeClass('showing')
+		$(thisElement).addClass('showing');
+	}
+}
 
+$('.menuListTabs').on('click', function(event){
+	event.preventDefault();
+	var elementIndex = $(this).index();
+	app.toggleArticles(elementIndex);
+});
 
 $('.select').change(function(){
 	$('.tile').remove();
@@ -32,44 +26,27 @@ $('.select').change(function(){
 		var results = response.results;
 		$.each(results, function(key, value){
 			var builders = value;
-			if (builders.area === areaVal){
-				$('.tileWrap').addClass("hasPics");
+			$('.tileWrap').addClass("hasPics");
 				$('#tileWrap').append(
-				'<div class="tile clearfix" id="tile"><div class="detailsWrap"><div class="name" id="name"><a target=" " href="' +
-				 builders.website + '">' + builders.name + '</a></div>' + '<div class="description" id="description">' +
-				 builders.description + '</div><br>' + 
-				'<div class="material" id="material">'
-				 + 'Materials:  ' + builders.material + '</div>' + 
-				 '<div class="specialism" id="specialism">'
-				 + 'Frame types:  ' + builders.specialism + '</div>' + 
-				'<div class="details" id="details">' + builders.location + 
-				'<br>' +  builders.phone + '<br><a href="mailto:' + builders.email + '">' 
-				+ builders.email + '</a></div>' + 
-				' <div class="website" id="website"><a target=" " href="' + builders.website + 
-				'">' + "Website" + 
-				' </a> </div></div><div class="pictures" id="pictures">' + 
-				 '<img src="' + builders.pictures + '">' + 
-				 '</div></div>'
-			)}
-			else if (areaVal === "the whole of the UK"){
-				$('.tileWrap').addClass("hasPics");
-				$('#tileWrap').append(
-				'<div class="tile clearfix" id="tile"><div class="detailsWrap"><div class="name" id="name"><a target=" " href="' +
-				 builders.website + '">' + builders.name + '</a></div>' + '<div class="description" id="description">' +
-				 builders.description + '</div><br>' + 
-				'<div class="material" id="material">'
-				 + 'Materials:  ' + builders.material + '</div>' + 
-				 '<div class="specialism" id="specialism">'
-				 + 'Frame types:  ' + builders.specialism + '</div>' + 
-				'<div class="details" id="details">' + builders.location + 
-				'<br>' +  builders.phone + '<br><a href="mailto:' + builders.email + '">' 
-				+ builders.email + '</a></div>' + 
-				' <div class="website" id="website"><a target=" " href="' + builders.website + 
-				'">' + "Website" + 
-				' </a> </div></div><div class="pictures" id="pictures">' + 
-				 '<img src="' + builders.pictures + '">' + 
-				 '</div></div>'
-				 )};
+				'<div class="tile clearfix" id="tile">'+
+					'<div class="detailsWrap">'+
+						'<div class="name" id="name">'+
+							'<a target=" " href="'+ builders.website + '">' + builders.name + '</a>'+
+						'</div>' + 
+						'<div class="description" id="description">'+ builders.description + '</div><br>' + 
+						'<div class="material" id="material"> Materials:  ' + builders.material + '</div>' + 
+				 		'<div class="specialism" id="specialism"> Frame types:  ' + builders.specialism + '</div>' + 
+						'<div class="details" id="details">' + builders.location + '<br>' +  builders.phone + '<br>'+
+							'<a href="mailto:' + builders.email + '">'+ builders.email + '</a>'+
+						'</div>' + 
+						'<div class="website" id="website">'+
+							'<a target=" " href="' + builders.website + '"> Website </a>'+
+						'</div>'+
+					'</div>'+
+					'<div class="pictures" id="pictures">' + 
+				 		'<img src="' + builders.pictures + '">' + 
+					 '</div>'+
+				'</div>');
 		});
 		$(document).scrollTop(300);
 	}});
